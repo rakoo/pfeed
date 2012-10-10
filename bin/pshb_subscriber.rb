a928518fe21243559db0570ffd9c0c8aa60e897e
rakoo <matthieu.rakotojaona@gmail.com>
2012-10-10 23:34:01 +0200

    Add pshb subscriber script
    Can receive new content

---
+++ bin/pshb_subscriber.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'couchrest'
+require 'base64'
+require 'pfeed'
+
+DBNAME = "http://localhost:5984/pfeed"
+
+post '/newcontent/:url' do |url|
+
+  Thread.new do
+    data = PFeed.parse_and_explode request.body.read
+    payload = {:docs => data.values.flatten}
+    CouchRest.post(DBNAME + "/_bulk_docs", payload)
+  end
+
+  204 # We have received the update; processing will be done by someone else, somewhere else.
+end
