d08cbbf977808ed4b4b6a3692ea9849723e5da12
Matthieu Rakotojaona <matthieu.rakotojaona@gmail.com>
2013-03-03 21:27:23 +0100

    Add timer to display nice progression

--- bin/cron_updater.rb
+++ bin/cron_updater.rb
@@ -8,10 +8,23 @@ DBNAME = "http://localhost:5984/pfeed"
 feeds_urls = DB.view("pfeed-couch/list-feeds-without-hubs")["rows"].map{|feed| feed["key"]}
 exit if feeds_urls.empty?
 
+puts "#{feeds_urls.size} feeds to update"
+
+breakpoint = Time.now.to_i
+done = 0
+
 parsed_feeds = feeds_urls.map do |url|
   modified_bits = PFeed.fetch_and_parse_explode(url)
   modified_bits.nil? ? nil : modified_bits.values
+
+  done += 1
+  if Time.now.to_i - breakpoint >= 2
+    breakpoint = Time.now.to_i
+    print "#{done} feeds parsed\r"
+  end
 end.compact.flatten
+
+puts "Done updating all feeds"
 
 payload = {:docs => parsed_feeds}
 CouchRest.post(DBNAME + "/_bulk_docs", payload)
